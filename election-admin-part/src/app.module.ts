60d4773dea4dc1b39a07b24ef45a889a2dae3014
tanvirgb <tspramanik123@gmail.com>
2024-03-15 22:51:19 +0600

    Added database connection

--- election-admin-part/src/app.module.ts
+++ election-admin-part/src/app.module.ts
@@ -2,9 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ElectionAdminModule } from './election-admin/election-admin.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ElectionAdminModule],
+  imports: [
+    ElectionAdminModule,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env.local' }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts/.js}'],
+        synchronize: configService.get<boolean>('DB_SYNC'),
+        autoLoadEntities: true,
+        logging: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- election-admin-part/src/election-admin/election-admin.module.ts
+++ election-admin-part/src/election-admin/election-admin.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ElectionAdminService } from './election-admin.service';
 import { ElectionAdminController } from './election-admin.controller';
+import { ElectionAdminProfile } from './entities/election-admin-profile.entity';
+import { ElectionAdmin } from './entities/election-admin.entity';
+import { ElectionAdminContact } from './entities/election-admin-contact.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Party } from './entities/party.entity';
+import { Report } from './entities/report.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      ElectionAdmin,
+      ElectionAdminProfile,
+      ElectionAdminContact,
+      Party,
+      Report,
+    ]),
+  ],
   controllers: [ElectionAdminController],
   providers: [ElectionAdminService],
 })
