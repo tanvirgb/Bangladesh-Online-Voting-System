31a23962319027bf6b61f9237197a867b7457ae8
Ramisa <120587298+Ramisa2101@users.noreply.github.com>
2024-03-24 23:46:07 +0600

    app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AdminModule } from './admin/admin.module';
-import { VoterModule } from './user/voter.module';
-import { User1Module } from './user1/user1.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { VotersModule } from './Voters/Voters.module';
 
